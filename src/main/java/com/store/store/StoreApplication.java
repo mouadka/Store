aa2e49784ef66073bce5d444f95ff69d26f6faef
mouadka <mouad.ka@gmail.com>
2022-09-15 12:43:52 +0200

    update

---
+++ src/main/java/com/store/store/StoreApplication.java
@@ -0,0 +1,16 @@
+package com.store.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+
+}
